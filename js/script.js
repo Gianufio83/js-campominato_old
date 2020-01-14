a6f54939eed756fa2a9fc66de2e92702d875c4c6
Gianufio83 <fiore.gianuario@gmail.com>
2020-01-14 16:23:20 +0100

    prime due parti dell' esercizio complete

--- js/script.js
+++ js/script.js
@@ -5,12 +5,12 @@
 // Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
 
 // Il computer deve generare 16 numeri casuali da 1 a 100.
-
+var arrayNumeriVietati = [];
 for (var i = 1; i <= 16 ; i++) {
   var genera = getRandomIntInclusive(100, 1);
-  console.log(genera);
+  arrayNumeriVietati.push(genera);
 }
-
+console.log(arrayNumeriVietati);
 function getRandomIntInclusive(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
@@ -20,13 +20,34 @@ function getRandomIntInclusive(min, max) {
 // In seguito deve chiedere all’utente di inserire un numero alla
 // volta, sempre compreso tra 1 e 100.
 
-for (var i = 1; i <=16; i++){
+// for (var i = 1; i <=4; i++){
+//   var numeroUtente = parseInt(prompt('Inserisci 16 numeri compresi tra 1 e 100'));
+//   var trovato = false;
+//   for (var j = 0; j < arrayNumeriVietati.length; j++) {
+//     if (numeroUtente == arrayNumeriVietati[j]) {
+//       trovato = true;
+//       console.log(trovato);
+//     } else {
+//
+//     }
+//   }
+// }
+var i = 0;
+var trovato = false;
+while (i < 4 && trovato == false) {
   var numeroUtente = parseInt(prompt('Inserisci 16 numeri compresi tra 1 e 100'));
-  console.log(numeroUtente);
+  for (var j = 0; j < arrayNumeriVietati.length; j++) {
+    if (numeroUtente == arrayNumeriVietati[j]) {
+      trovato = true;
+      console.log(trovato);
+    }
+  }
+  i++;
 }
-var arrayNumeri = [];
-arrayNumeri.push(numeroUtente);
-console.log(arrayNumeri);
+// var arrayNumeri = [];
+// arrayNumeri.push(numeroUtente);
+// console.log(arrayNumeri);
+
 // var numeriUtente = getUserNumber(numeri);
 // console.log(numeri);
 
@@ -40,10 +61,13 @@ console.log(arrayNumeri);
 
 // se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti continua chiedendo all’utente un altro numero.
 // La partita termina quando il giocatore inserisce un numero “vietato”, ovvero presente nella lista di numeri random, o raggiunge il numero massimo possibile di tentativi consentiti.
-var tentativi = 3;
-var nuovoTentativo = parseInt(prompt('Inserisci un altro numero compreso tra 1 e 100'));
-if (genera == numeroUtente || tentativi != 3) {
-  console.log('Termine della partita');
-} else {
-  console.log(nuovoTentativo);
-}
+
+// var tentativi = 3;
+//
+// if (genera == numeroUtente || tentativi != 3) {
+//   console.log('Termine della partita');
+// } else {
+//   var nuovoTentativo = parseInt(prompt('Inserisci un altro numero compreso tra 1 e 100'));
+//   console.log(nuovoTentativo);
+// }
+// Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
